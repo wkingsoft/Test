619c53dbb809515c63372d8f02447fa18a3baa20
wkingsoft@126.com <wkingsoft@126.com>
2015-06-25 00:26:33 +0800

    c

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld");
 		String str = null;
 		System.out.println(str);
 	}
